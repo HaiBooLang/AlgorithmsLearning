c56154a4db7b850cdf4035848e653c8e5965c80e
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-11-14 11:13:28 +0800

    优化 BinarySearch

--- Search/BinarySearch.h
+++ Search/BinarySearch.h
@@ -1,9 +1,40 @@
 #include <iterator>
+#include <vector>
+#include <array>
+#include <list>
+#include <deque>
 
 namespace Search
 {
 
-    template<class InputIt, class T>
+    template<std::ranges::random_access_range Container, class T>
+    constexpr auto binary_search(Container& c, const T& value)
+    {
+        auto first = std::begin(c);
+        auto last = std::end(c);
+
+        while (first != last)
+        {
+            auto mid = std::next(first, std::distance(first, last) / 2);
+
+            if (*mid == value)
+            {
+                return mid;
+            }
+            else if (*mid < value)
+            {
+                first = std::next(mid);
+            }
+            else
+            {
+                last = mid;
+            }
+        }
+
+        return last;
+    }
+
+    template<std::random_access_iterator InputIt, class T>
     constexpr InputIt binary_search(InputIt first, InputIt last, const T& value)
     {
         while (first != last)
@@ -26,5 +57,35 @@ namespace Search
 
         return last;
     }
+
+    //template<class T, std::size_t N>
+    //constexpr T* binary_search(T(&arr)[N], const T& value)
+    //{
+    //    return binary_search(std::begin(arr), std::end(arr), value);
+    //}
+
+    //template<class T>
+    //constexpr typename std::vector<T>::iterator binary_search(std::vector<T>& vec, const T& value)
+    //{
+    //    return binary_search(vec.begin(), vec.end(), value);
+    //}
+
+    //template<class T, std::size_t N>
+    //constexpr typename std::array<T, N>::iterator binary_search(std::array<T, N>& arr, const T& value)
+    //{
+    //    return binary_search(arr.begin(), arr.end(), value);
+    //}
+
+    //template<class T>
+    //constexpr typename std::list<T>::iterator binary_search(std::list<T>& lst, const T& value)
+    //{
+    //    return binary_search(lst.begin(), lst.end(), value);
+    //}
+
+    //template<class T>
+    //constexpr typename std::deque<T>::iterator binary_search(std::deque<T>& deq, const T& value)
+    //{
+    //    return binary_search(deq.begin(), deq.end(), value);
+    //}
 
 }
--- Search/TestSearch.cpp
+++ Search/TestSearch.cpp
@@ -1,5 +1,6 @@
-#include "TestSearch.h"
+#include <numeric>
 
+#include "TestSearch.h"
 #include "../MeasureTime.h"
 
 namespace Search
@@ -11,8 +12,7 @@ namespace Search
     }
 
     void test_binary_search(int size) {
-        std::random_device rd;
-        std::mt19937 gen(rd());
+        std::mt19937 gen(std::random_device{}());
         std::uniform_int_distribution<> dist(1, size);
 
         std::vector<int> data(size);
@@ -22,16 +22,16 @@ namespace Search
 
         std::sort(data.begin(), data.end());
 
-        auto worst_case_time = measure_time(binary_search<std::vector<int>::iterator, int>, data.begin(), data.end(), size + 1);
+        auto worst_case_time = measure_time(binary_search<std::vector<int>, int>, data, size + 1);
         std::cout << "BinarySearch Worst case time: " << worst_case_time << " ns\n";
 
-        auto best_case_time = measure_time(binary_search<std::vector<int>::iterator, int>, data.begin(), data.end(), data[size / 2]);
+        auto best_case_time = measure_time(binary_search<std::vector<int>, int>, data, data[size / 2]);
         std::cout << "BinarySearch Best case time: " << best_case_time << " ns\n";
 
         long long total_time = 0;
         for (int i = 0; i < 10; ++i) {
             int target = dist(gen);
-            total_time += measure_time(binary_search<std::vector<int>::iterator, int>, data.begin(), data.end(), target);
+            total_time += measure_time(binary_search<std::vector<int>, int>, data, target);
         }
         std::cout << "BinarySearch Average case time: " << total_time / 10 << " ns\n";
     }

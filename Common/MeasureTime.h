6dcbc4756891f3bbf727c795785b6c442705c9e8
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-11-14 13:34:34 +0800

    扩展 MeasureTime

--- Common/MeasureTime.h
+++ Common/MeasureTime.h
@@ -1,17 +1,45 @@
 #pragma once
 
+#include <vector>
+#include <functional>
 #include <chrono>
+#include <iostream>
+#include <algorithm>
+#include <string>
 
 template<typename Func, typename... Args>
 auto measure_time(Func&& func, Args&&... args) {
-    auto start = std::chrono::high_resolution_clock::now();
+	auto start = std::chrono::high_resolution_clock::now();
 
-    std::forward<Func>(func)(std::forward<Args>(args)...);
+	std::forward<Func>(func)(std::forward<Args>(args)...);
 
-    auto end = std::chrono::high_resolution_clock::now();
+	auto end = std::chrono::high_resolution_clock::now();
 
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
 
-    return duration.count();
+	return duration.count();
 }
 
+template<typename DataType>
+void compare_functions(const std::vector<std::pair<std::string, std::function<void(const DataType&)>>>& functions, const DataType& data) {
+	struct TimePair {
+		std::string name;
+		decltype(std::chrono::nanoseconds().count()) time;
+	};
+
+	std::vector<TimePair> times;
+	times.reserve(functions.size());
+
+	for (const auto& func : functions) {
+		auto time = measure_time(func.second, data);
+		times.push_back({ std::move(func.first), time });
+	}
+
+	std::sort(times.begin(), times.end(), [](const TimePair& a, const TimePair& b) {
+		return a.time < b.time;
+		});
+
+	for (const auto& time : times) {
+		std::cout << "Function: " << time.name << ", Time taken: " << time.time << " ns\n";
+	}
+}

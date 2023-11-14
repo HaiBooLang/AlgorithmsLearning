db553448d4dcfa53d863376da89c79e67c2f4056
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-11-14 13:47:08 +0800

    扩展 Stub

--- Common/Stub.h
+++ Common/Stub.h
@@ -5,12 +5,36 @@
 namespace std {
 
     int swap_count = 0;
+    int compare_count = 0;
+    int increment_count = 0;
+    int decrement_count = 0;
 
     template<typename Iterator>
     void iter_swap(Iterator a, Iterator b)
     {
         std::swap(*a, *b);
         ++swap_count;
+    }
+
+    template<typename T>
+    bool less(const T& a, const T& b)
+    {
+        ++compare_count;
+        return a < b;
+    }
+
+    template<typename Iterator>
+    Iterator next(Iterator it)
+    {
+        ++increment_count;
+        return std::next(it);
+    }
+
+    template<typename Iterator>
+    Iterator prev(Iterator it)
+    {
+        ++decrement_count;
+        return std::prev(it);
     }
 
 }

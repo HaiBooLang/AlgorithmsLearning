cd12a062321e3a48b5cdc07f07ef8a6ec6f145da
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-11-19 00:10:04 +0800

    完善 MeasureTime.h
    1. 修复 BUG，测试数据集直接传入待测试函数而未创建临时副本导致传入已排序数据集
    2. 优化测试数据输出

--- Common/MeasureTime.h
+++ Common/MeasureTime.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <typeinfo>
+#include <map>
 #include <vector>
 #include <functional>
 #include <chrono>
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <iomanip>
+#include <cassert>
 
 #include "DataSets.h"
 
@@ -33,9 +37,11 @@ inline void compare_functions(const std::vector<std::pair<std::string, std::func
 	records.reserve(functions.size());
 
 	for (const auto& func : functions) {
-		long long time = 0;
+		decltype(std::chrono::nanoseconds().count()) time = 0;
 		for (int i = 0; i < times; ++i) {
-			time += measure_time(func.second, data);
+			DataType temp_data = data;
+			time += measure_time(func.second, temp_data);
+			assert(std::is_sorted(std::begin(temp_data), std::end(temp_data)));
 		}
 		records.push_back({ std::move(func.first), time/times });
 	}
@@ -46,8 +52,37 @@ inline void compare_functions(const std::vector<std::pair<std::string, std::func
 
 	std::cout << std::left << std::setw(20) << "Function" << "Time taken (ns)\n";
 	for (const auto& record : records) {
-		std::cout << std::left << std::setw(20) << record.name << record.time << "\n";
+		std::cout << std::left << std::setw(20) << record.name 
+			<< std::setw(20) << record.time 
+			<< std::setw(20) << 100.0 * record.time / records[0].time << "\n";
 	}
+}
+
+
+template<typename T>
+inline std::string get_type_name() {
+	static const std::map<std::string, std::string> type_names = {
+		{typeid(std::vector<int>).name(), "vector<int>"},
+		{typeid(std::vector<double>).name(), "vector<double>"},
+		{typeid(std::vector<float>).name(), "vector<float>"},
+	};
+
+	auto iterator = type_names.find(typeid(T).name());
+	if (iterator != type_names.end()) {
+		return iterator->second;
+	}
+	else {
+		return typeid(T).name();
+	}
+}
+
+
+inline std::tm get_current_time() {
+	auto current_time_point = std::chrono::system_clock::now();
+	std::time_t current_time_time_t = std::chrono::system_clock::to_time_t(current_time_point);
+	std::tm current_time_struct;
+	localtime_s(&current_time_struct, &current_time_time_t);
+	return current_time_struct;
 }
 
 template<typename DataType>
@@ -60,11 +95,16 @@ inline void compare_with_all_datasets(const std::vector<std::pair<std::string, s
 		{"few unique", generate_few_unique_data},
 	};
 
+	std::tm current_time_struct = get_current_time();
+
+	std::cout << std::left << std::setw(20) << "Current time: " << std::put_time(&current_time_struct, "%Y-%m-%d %H:%M:%S") << "\n"
+		<< std::left << std::setw(20) << "Data size: " << size << " " << get_type_name<DataType>() << "\n";
+
 	for (const auto& [dataset_name, generate_data] : datasets) {
 		std::cout << "========================================\n\n";
 		std::cout << "Testing with " << dataset_name << " data:\n";
 		DataType data = generate_data(size);
 		compare_functions(functions, data, times);
-		std::cout << "\n";
+		std::cout << std::endl;
 	}
 }
